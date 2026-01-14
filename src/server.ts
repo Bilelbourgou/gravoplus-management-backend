55f4bf5054b652b21b2c1b7e92a76df612db0ee8
Bilelbourgou <bilelbourgou@gmail.com>
2026-01-14 17:56:00 +0100

    fix cors origins

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ app.set('io', io);
 // Middleware
 app.use(helmet());
 app.use(cors({
-    origin: config.env === 'development' ? true : config.cors.origin,
+    origin: '*',
     credentials: true,
 }));
 app.use(express.json());
